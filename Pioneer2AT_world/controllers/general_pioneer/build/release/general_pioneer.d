f6a813c0339a2150b72188c0971af9d3ec864a2f
vgutiupm <68591845+vgutiupm@users.noreply.github.com>
2020-08-10 17:58:48 +0200

    Add files via upload

---
+++ Pioneer2AT_world/controllers/general_pioneer/build/release/general_pioneer.d
@@ -0,0 +1,9 @@
+build/release/general_pioneer.o: general_pioneer.c \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/position_sensor.h \
+ /usr/local/webots/include/controller/c/webots/lidar.h \
+ /usr/local/webots/include/controller/c/webots/lidar_point.h \
+ /usr/local/webots/include/controller/c/webots/motor.h
