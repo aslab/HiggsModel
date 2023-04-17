33176caffed8661c561c82b1b49ad7cb4dbb90db
AlvaroBarry <abarrigonfer@gmail.com>
2023-04-17 15:28:02 +0200

    first draft foxy

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,105 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='higgs2' #<--- CHANGE ME
+
+
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                    #<--- CHANGE CONTROL MODE BETWEEN ROS2 OR SKID use_ros2_control
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+    
+    # Run rviz2 node with view
+    rviz2 = Node(package='rviz2', executable='rviz2', name="rviz2", output='screen', 
+                 arguments=['-d',[os.path.join(get_package_share_directory(package_name),'config','view_bot.rviz')],' -p', ' use_sim_time:=true'])
+
+
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+#    relay_odom = Node(
+#     name="relay_odom",
+#     package="topic_tools",
+#     executable="relay",
+#     parameters=[
+#         {
+#             "input_topic": "/diff_cont/odom",
+#             "output_topic": "/odom",
+#         }
+#     ],
+#     output="screen",
+#     )
+    
+    
+
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        joystick,
+        twist_mux,
+        gazebo,
+        spawn_entity,
+        #relay_odom,
+        diff_drive_spawner,
+        joint_broad_spawner,
+        rviz2
+    ])
---
+++ launch/rsp.launch.py
@@ -0,0 +1,53 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ParameterValue  # Need master or Galactic branch for this feature
+
+import xacro
+
+
+def generate_launch_description():
+
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
+
+    # Process the URDF file
+    pkg_path = os.path.join(get_package_share_directory('higgs2'))
+    xacro_file = os.path.join(pkg_path,'src','description','robot.urdf.xacro')
+    #robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_description_config = Command(["xacro ", xacro_file, " use_ros2_control:=", use_ros2_control, " sim_mode:=", use_sim_time])
+
+    
+    # Create a robot_state_publisher node
+    #params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        # parameters=[params]
+        parameters=[{"robot_description": ParameterValue(robot_description_config, value_type=str),
+                     "use_sim_time": use_sim_time}]
+    )
+
+
+    # Launch!
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use sim time if true'),
+        DeclareLaunchArgument(
+            'use_ros2_control',
+            default_value='true',
+            description='Use ros2_control if true'),
+
+        node_robot_state_publisher
+    ])
