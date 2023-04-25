eff9d8934df010a99050d2a0df0f021dabbe2f47
AlvaroBarry <abarrigonfer@gmail.com>
2023-04-25 17:53:21 +0200

    nav2 working

--- launch/localization_launch.py
+++ launch/localization_launch.py
@@ -64,7 +64,7 @@ def generate_launch_description():
 
         DeclareLaunchArgument(
             'map',
-            default_value=os.path.join(bringup_dir, 'config','maps', 'laberinto_map_save.yaml'),
+            default_value=os.path.join(bringup_dir, 'maps', 'turtlebot3_world.yaml'),
             description='Full path to map yaml file to load'),
 
         DeclareLaunchArgument(
