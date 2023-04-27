f3c7ae7d908b05537d9e94e4383c5fad737f7865
AlvaroBarry <abarrigonfer@gmail.com>
2023-04-27 18:44:17 +0200

    joystick changes working

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -29,11 +29,11 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    #  joystick = IncludeLaunchDescription(
-    #              PythonLaunchDescriptionSource([os.path.join(
-    #                  get_package_share_directory(package_name),'launch','joystick.launch.py'
-    #              )])
-    #  )
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )])
+    )
 
 
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
@@ -108,8 +108,8 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        #joystick,
-        #twist_mux,
+        joystick,
+        twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
